9852631394184ac24449db93fe71571c1dbe2477
msadecki <marcin.sadecki@gmail.com>
2026-02-13 17:04:26 +0100

    New Scaffolded Item - Blazor - Razor Components using Entity Framework (CRUD) - CustomTask - build failed

--- BlazorApp01.Web/Program.cs
+++ BlazorApp01.Web/Program.cs
@@ -1,5 +1,6 @@
-using BlazorApp01.Web.Components;
+﻿using BlazorApp01.Web.Components;
 using BlazorApp01.DataAccess;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -19,6 +20,7 @@ if (!app.Environment.IsDevelopment())
     app.UseExceptionHandler("/Error", createScopeForErrors: true);
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
+    app.UseMigrationsEndPoint();
 }
 app.UseStatusCodePagesWithReExecute("/not-found", createScopeForStatusCodePages: true);
 app.UseHttpsRedirection();
