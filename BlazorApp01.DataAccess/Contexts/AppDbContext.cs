8761f9ccc26f5553a31dcf4edafa846b05001148
msadecki <marcin.sadecki@gmail.com>
2026-02-13 17:04:29 +0100

    ModifyDateTimeColumnsPrecision

--- BlazorApp01.DataAccess/Contexts/AppDbContext.cs
+++ BlazorApp01.DataAccess/Contexts/AppDbContext.cs
@@ -7,4 +7,13 @@ namespace BlazorApp01.DataAccess.Contexts;
 public sealed class AppDbContext(DbContextOptions<AppDbContext> options) : IdentityDbContext<ApplicationUser>(options)
 {
     public DbSet<CustomTask> CustomTasks { get; set; }
+
+    protected override void ConfigureConventions(ModelConfigurationBuilder builder)
+    {
+        builder.Properties<DateTime>()
+               .HaveColumnType("datetime2(0)");
+
+        builder.Properties<DateTime?>()
+               .HaveColumnType("datetime2(0)");
+    }
 }
---
+++ BlazorApp01.DataAccess/Migrations/20260206164037_ModifyDateTimeColumnsPrecision.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace BlazorApp01.DataAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class ModifyDateTimeColumnsPrecision : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "CustomTasks",
+                type: "datetime2(0)",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CompletionDate",
+                table: "CustomTasks",
+                type: "datetime2(0)",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2",
+                oldNullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CreatedAt",
+                table: "CustomTasks",
+                type: "datetime2",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2(0)");
+
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "CompletionDate",
+                table: "CustomTasks",
+                type: "datetime2",
+                nullable: true,
+                oldClrType: typeof(DateTime),
+                oldType: "datetime2(0)",
+                oldNullable: true);
+        }
+    }
+}
